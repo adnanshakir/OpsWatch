d62b2df2979235eb29afe23aae8044a74699e2bb
harshwardhan12 <lalitasolanki2577@gmail.com>
2026-04-29 19:10:01 +0530

    feat(security): add helmet, rate limiting, request sanitization, and logging

--- server/src/app.js
+++ server/src/app.js
@@ -1,29 +1,89 @@
 import express from 'express';
-import morgan from 'morgan';
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
 import authRoutes from './routes/auth.routes.js';
 import incidentRoutes from './routes/incident.routes.js';
 import errorHandler from './middlewares/error.middleware.js';
 import passport from './config/passport.js';
+import { globalLimiter } from './middlewares/rateLimit.middleware.js';
+import helmet from 'helmet';
+import mongoSanitize from '@exortek/express-mongo-sanitize';
+import { xss } from 'express-xss-sanitizer';
+import pinoHttp from 'pino-http';
+import { apiLimiter } from './middlewares/rateLimit.middleware.js';
+import { authenticate } from './middlewares/auth.middleware.js';
+import { config } from './config/config.js';
 
 const app = express();
 
-// Middleware
-app.use(morgan('dev'));
-app.use(express.json());
-app.use(cookieParser());
+app.set('trust proxy', 1);
+
+// Security headers
+app.use(
+  helmet({
+    crossOriginResourcePolicy: false,
+  })
+);
+
+// CORS
 app.use(
   cors({
-    origin: 'http://localhost:5173',
+    origin: config.FRONTEND_URL,
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     credentials: true,
   })
 );
+
+// Logger
+app.use(
+  pinoHttp({
+    transport: {
+      target: 'pino-pretty',
+      options: {
+        colorize: true,
+        ignore: 'pid,hostname,req,res', // 👈 removes big clutter
+      },
+    },
+    redact: ['req.headers.cookie', 'req.headers.authorization'],
+    customSuccessMessage: (req, res) =>
+      `${req.method} ${req.url} -> ${res.statusCode}`,
+
+    customErrorMessage: (req, res, err) =>
+      `${req.method} ${req.url} -> ${res.statusCode} ❌`,
+  })
+);
+
+// Health check route
+app.get('/health', (req, res) => {
+  res.status(200).json({
+    status: 'OK',
+    uptime: process.uptime(),
+    timestamp: new Date().toISOString(),
+  });
+});
+
+// Rate limiting
+app.use(globalLimiter);
+
+// Body parsers
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+// Sanitization
+app.use(
+  mongoSanitize({
+    replaceWith: '_',
+  })
+);
+app.use(xss());
+
+// Auth init
 app.use(passport.initialize());
 
 // Routes
 app.use('/api/auth', authRoutes);
+app.use('/api', authenticate, apiLimiter);
 app.use('/api/incidents', incidentRoutes);
 
 // Central error handling
